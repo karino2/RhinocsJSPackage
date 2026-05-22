fbfbd265a99d3ff688312e886d14b4de2ae6f188
Kazuma Arino <hogeika2@gmail.com>
2026-05-22 18:35:55 +0900

    skkのロードが無言だと何が起きているのかわかりにくいので最初にtoastを出すようにする。

--- init.js
+++ init.js
@@ -1,7 +1,9 @@
 request_load_js("/file_history/file_history.js");
+
 // skk_all.jsは時間がかかるのでlazyにロード
 // request_load_js("/skk/skk_all.js");
 global_set_key(["C-x", "C-j"], () => {
+    show_toast("Loading SKK...");
     request_load_js("/skk/skk_all.js", ()=> {
         g_skk.toggleEnableSKK();
     });
