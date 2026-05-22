b3a37dd701bc70eccc72286c4ed9414d12665ee1
Kazuma Arino <hogeika2@gmail.com>
2026-05-22 18:22:49 +0900

    init.jsにコメントを追加

--- init.js
+++ init.js
@@ -1,6 +1,6 @@
-print("init js loading.");
-// request_load_js("/skk/skk_all.js");
 request_load_js("/file_history/file_history.js");
+// skk_all.jsは時間がかかるのでlazyにロード
+// request_load_js("/skk/skk_all.js");
 global_set_key(["C-x", "C-j"], () => {
     request_load_js("/skk/skk_all.js", ()=> {
         g_skk.toggleEnableSKK();
