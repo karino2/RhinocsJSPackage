25c89fde92c35f407147a2a6c2f425e3ad0205eb
Kazuma Arino <hogeika2@gmail.com>
2026-05-30 23:07:42 +0900

    todayを実装してみる
    良く日記などで使っているのでinit.jsに入れておく。

--- init.js
+++ init.js
@@ -2,9 +2,29 @@ load_js("/file_history/file_history.js");
 
 // skk_all.jsは時間がかかるのでlazyにロード
 // load_js("/skk/skk_all.js");
+/*
+load_js("/skk/skk_all.js").then(()=> {
+    g_skk.toggleEnableSKK();
+});
+*/
 global_set_key(["C-x", "C-j"], () => {
     show_toast("Loading SKK...");
     load_js("/skk/skk_all.js").then(()=> {
         g_skk.toggleEnableSKK();
     });
 });
+
+
+function today() {
+    let date = new Date();
+    let weekdays = ["日", "月", "火", "水", "木", "金", "土"];
+
+    let yyyy = date.getFullYear();
+    let mm = ("0" + (date.getMonth() + 1)).slice(-2);
+    let dd = ("0" + date.getDate()).slice(-2);
+    let ddd = weekdays[date.getDay()];
+
+    let formatted = yyyy + "-" + mm + "-" + dd + " (" + ddd + ")";
+
+    insert(formatted);
+}
--- misc.js
+++ misc.js
@@ -1,4 +1,17 @@
+let buf = get_buffer_create("*scratch*");
+switch_to_buffer(buf);
+
 // eval_region用
 set_device_id("BOOX");
 set_device_id("MotoG53y");
 message(get_device_id());
+
+
+read_string("Hoge: ").then((str)=>{show_toast(str)});
+
+
+
+select_file(["*/*"]).then(([uri, fname])=>print(`${uri}, ${fname}`));
+
+var buf = get_buffer_create("*tmp*");
+print(buf.name)
